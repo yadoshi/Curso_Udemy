dab18c7c7aa5bbb6ce2f2479ec3839fa810e71d3
yadoshi <yagodfernandes@gmail.com>
2023-05-22 11:54:46 -0300

    Revert "adicionando enumeracao"
    This reverts commit 4a8306759d53f358e17165831827a04c53a92a59.

--- udemy/src/application/Negativo.java
+++ udemy/src/application/Negativo.java
@@ -3,27 +3,26 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Program {
+public class Negativo {
 
 	public static void main(String[] args) {
+		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
 		int n = sc.nextInt();
-		double[] vect = new double[n];
+		
+		int[] vect = new int[n];
 		
 		for (int i = 0; i<n; i++) {
-			vect[i] = sc.nextDouble();
+			vect[i] = sc.nextInt();
 		}
 		
-		double sum = 0.0;
-		for (int  i=0; i<n; i++) {
-			sum += vect[i];
+		for (int i = 0; i<n; i++) {
+			if (vect[i] < 0) {
+				System.out.println(vect[i]);
+			}
 		}
-		double avg = sum / n;
-		
-		System.out.printf("%.2f%n", avg);
-		
 		sc.close();
 	}
 
--- udemy/src/application/Numeros_pares.java
+++ udemy/src/application/Numeros_pares.java
@@ -3,27 +3,27 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Program {
+public class Numeros_pares {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
 		int n = sc.nextInt();
-		double[] vect = new double[n];
+		int[] vect = new int[n];
 		
 		for (int i = 0; i<n; i++) {
-			vect[i] = sc.nextDouble();
+			vect[i] = sc.nextInt();
 		}
 		
-		double sum = 0.0;
-		for (int  i=0; i<n; i++) {
-			sum += vect[i];
+		int qtdpares = 0;
+		for (int i = 0; i<n; i++) {
+			if (vect[i] % 2 == 0) {
+				System.out.println(vect[i]);
+				qtdpares++;
+			}
 		}
-		double avg = sum / n;
-		
-		System.out.printf("%.2f%n", avg);
-		
+		System.out.println(qtdpares);
 		sc.close();
 	}
 
